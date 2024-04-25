b0a0e7d9fcbab2d728eba803fdeaa2f5073f64f6
Natanael Rodrigues <natanaelrodrigues@gmail.com>
2024-04-25 16:21:59 -0300

    first commit

---
+++ fundamentos/ola-mundo.py
@@ -0,0 +1,11 @@
+print('Ola Mundo!')
+print('Ola Mundo novamente!')
+
+# Variaveis
+faturamento = 5500.5 
+custo = 1350
+lucro = faturamento - custo
+
+print('Faturamento foi de ', faturamento)
+print('Custo foi de ', custo)
+print('Lucro foi de ', lucro)
